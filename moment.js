d9b4c4fafd0cd55f62999dcb32cffa41b29e3482
Tim Wood <washwithcare@gmail.com>
2013-03-07 20:34:06 -0800

    zones outputting correct timestamped date with toDate

--- moment.js
+++ moment.js
@@ -1111,7 +1111,7 @@
         },
 
         toDate : function () {
-            return this._d;
+            return this._offset ? new Date(+this) : this._d;
         },
 
         toJSON : function () {
